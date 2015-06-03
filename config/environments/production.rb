270def6679cff208ec23c04d464b607d5d670b96
Deborah A. Anderson <deborahaanderson@gmail.com>
2015-06-02 22:48:19 -0500

    had wrong host in prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'secret-beach-5350.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'dry-brook-9112.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :enable_starttls_auto => true,
